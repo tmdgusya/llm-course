27918c5618d26370adc9f9db5ceef54e10644690
정승현 <tmdgusya@naver.com>
2023-06-09 12:49:21 +0900

    prompt template

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 import openai
 
 from dotenv import load_dotenv, find_dotenv
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
 
 _ = load_dotenv(find_dotenv())  # read local .env file
 openai.api_key = os.environ['OPENAI_API_KEY']
@@ -28,14 +30,62 @@ cleaning up me kitchen. I need yer help \
 right now, matey!
 """
 
-style = """American English \
+customer_style = """American English \
 in a calm and respectful tone
 """
 
 prompt = f"""Translate the text \
 that is delimited by triple backticks
-into a style that is {style}.
+into a style that is {customer_style}.
 text: ```{customer_email}```
 """
 
-print(get_completion(prompt))
+# print(get_completion(prompt))
+
+chat = ChatOpenAI(temperature=0.0)
+
+template_string = """Translate the text \
+that is delimited by triple backticks \
+into a style that is {style}. \
+text: ```{text}```
+"""
+
+prompt_template = ChatPromptTemplate.from_template(template_string)
+print(prompt_template.messages[0].prompt)  # show template and variables
+print(prompt_template.messages[0].prompt.input_variables)  # show variables
+
+customer_messages = prompt_template.format_messages(
+    style=customer_style,
+    text=customer_email,
+)
+
+print(customer_messages[0])
+
+# customer_response = chat(customer_messages)
+# print(customer_response.content)
+
+service_reply = """Hey there customer, \
+the warranty does not cover \
+cleaning expenses for your kitchen \
+because it's your fault that \
+you misused your blender \
+by forgetting to put the lid on before \
+starting the blender. \
+Tough luck! See ya!
+"""
+
+service_style_pirate = """\
+a polite tone \
+that speaks in English Pirate\
+"""
+
+# reuse prompt template
+service_messages = prompt_template.format_messages(
+    style=service_style_pirate,
+    text=service_reply
+)
+
+print(service_messages[0].content)
+
+service_response = chat(service_messages)
+print(service_response.content)
